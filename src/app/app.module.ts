edfb5966193c6e65519a11da229fc1e9253bbafc
Gabriel <68778251+GBRL-Web@users.noreply.github.com>
2023-01-11 15:45:02 +0100

    docs: Initial commit. :construction:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './commons/navbar/navbar.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { NotfoundComponent } from './commons/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
